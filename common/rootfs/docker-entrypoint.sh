77ce051f45d90cc5833fcb3debe98cfcb4fbd3d2
Virusmater <virusmater@gmail.com>
2024-06-15 20:17:03 +0200

    Update docker-entrypoint.sh

--- common/rootfs/docker-entrypoint.sh
+++ common/rootfs/docker-entrypoint.sh
@@ -84,7 +84,7 @@ function export_config() {
 export_config 'mqtt'
 export_config 'serial'
 
-if bashio::config.is_empty 'mqtt' && bashio::var.has_value "$(bashio::services 'mqtt')"; then
+if bashio::config.is_empty 'mqtt.server' && bashio::var.has_value "$(bashio::services 'mqtt')"; then
     if bashio::var.true "$(bashio::services 'mqtt' 'ssl')"; then
         export ZIGBEE2MQTT_CONFIG_MQTT_SERVER="mqtts://$(bashio::services 'mqtt' 'host'):$(bashio::services 'mqtt' 'port')"
     else
@@ -92,6 +92,9 @@ if bashio::config.is_empty 'mqtt' && bashio::var.has_value "$(bashio::services '
     fi
     export ZIGBEE2MQTT_CONFIG_MQTT_USER="$(bashio::services 'mqtt' 'username')"
     export ZIGBEE2MQTT_CONFIG_MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
+fi
+
+if bashio::config.has_value 'mqtt.base_topic'; then
     export ZIGBEE2MQTT_CONFIG_MQTT_BASE_TOPIC="$(bashio::services 'mqtt' 'base_topic')"
 fi
 
